ad6ecd86ab14a4351460628e7a6b227e556aa8ea
ross@linux <orz_amber@163.com>
2021-04-07 11:15:00 +0800

    add OFB

--- AES/AES.c
+++ AES/AES.c
@@ -663,7 +663,7 @@ int aes_encrypt_cbc(AES_CYPHER_T mode, uint8_t *data, int len, uint8_t *key, uin
         {
             break;
         }
-        len_tmp -= 16; //control the circle
+        len_tmp -= 16; //control the loop
         d += 16; //pointer address add
         i += 16; //pointer address add
     }
@@ -825,6 +825,32 @@ int aes_encrypt_cfb8(AES_CYPHER_T mode, uint8_t *data, int len, uint8_t *key,
     return ret;
 }
 
+/**
+ * AES OFB
+ * do remember this mode requires iv is a nonce.
+ */
+int aes_encrypt_ofb(AES_CYPHER_T mode, uint8_t *data, int len, uint8_t *key,
+                    uint8_t *iv)
+{
+    int ret, l = 0;
+    uint8_t *iv_tmp = malloc(16);
+    memcpy(iv_tmp, iv, 16);
+
+    while (l < len)
+    {
+        aes_encrypt(mode, iv_tmp, key);
+        for (int i = 0; i < 16 && l + i < len; i++)
+        {
+            data[l + i] ^= iv_tmp[i];
+        }
+        l += 16;
+    }
+
+    free(iv_tmp);
+
+    return ret;
+}
+
 int main1()
 {
     printf("%02x", aes_mul(0x57, 0x83));
@@ -956,7 +982,7 @@ int main()
     uint8_t *anni_buf = malloc(sizeof(buf));
     memcpy(anni_buf, buf, sizeof(buf));
     int len = sizeof(buf);
-    BytePadding(anni_buf, &len);
+    BytePadding(&anni_buf, &len);
     // return 0;
     uint8_t anni_key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
                           0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
@@ -1007,8 +1033,9 @@ int main()
 
 #endif
 
-#define AES_CFB
+// #define AES_CFB
 #ifdef AES_CFB
+#include "test.h"
     int num = 0;
     uint8_t anni_buf1[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
                            0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
@@ -1055,9 +1082,48 @@ int main()
     // }
     // printf("\n");
 
+    // int len = sizeof(pic);
+    // uint8_t *anni_buf = malloc(len);
+    // memcpy(anni_buf, pic, len);
+    // printf("len is %d\n", len);
+
     aes_encrypt_cfb8(AES_CYPHER_128, anni_buf1, sizeof(anni_buf1), key, iv, 1);
     aes_encrypt_cfb8(AES_CYPHER_128, anni_buf1, sizeof(anni_buf1), key, iv, 0);
+
+    // FILE *fp;
+    // fp = fopen("./out3.txt", "w");
+
+    // for (int i = 0; i < sizeof(pic); i += 3)
+    // {
+    //     fprintf(fp, "%d, %d, %d\n", anni_buf[i], anni_buf[i + 1], anni_buf[i + 2]);
+    // }
+    // printf("\n");
+    // fclose(fp);
     
+#endif
+
+#define AES_OFB
+#ifdef AES_OFB
+    uint8_t anni_buf[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+                          0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+                          0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c,
+                          0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51,
+                          0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11,
+                          0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
+                          0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17,
+                          0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c,};// 0x37, 0x10};
+
+    // uint8_t anni_buf[] = "this is a test messageeee...";
+
+    uint8_t key[]      = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+                          0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+
+    uint8_t iv[]       = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                          0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+    aes_encrypt_ofb(AES_CYPHER_128, anni_buf, sizeof(anni_buf), key, iv); //enc
+    aes_encrypt_ofb(AES_CYPHER_128, anni_buf, sizeof(anni_buf), key, iv); //dec
+
 #endif
 
     return 0;
