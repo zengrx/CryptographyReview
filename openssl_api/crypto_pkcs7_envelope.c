364ae007609abe5c9b1cca62882e6a3eef5b6974
ross <orz_amber@163.com>
2023-01-28 01:27:18 +0800

    Feat: add pkcs7 envelope open

--- openssl_api/crypto_pkcs7_envelope.c
+++ openssl_api/crypto_pkcs7_envelope.c
@@ -59,12 +59,12 @@ X509 *parse_cert(char * cert_file /*, EVP_PKEY *pkey */)
 
 parse_cert_err:
 	//your error handle
-	printf("err out\n");
+	printf("%s err out\n", __func__);
 	cert = NULL;
 	return cert;
 }
 
-int seal_pkcs7_envelop(X509 *cert, char *fin, char *fout, void *msg)
+int seal_pkcs7_envelope(X509 *cert, char *fin, char *fout, void *msg)
 {
 	int ret, f_len = 0;
 	FILE *fp = NULL;
@@ -81,7 +81,7 @@ int seal_pkcs7_envelop(X509 *cert, char *fin, char *fout, void *msg)
 	//use aes 128 cbc as default, the iv at enc_data-algorithm-parameter
 	p7 = PKCS7_encrypt(certs, b_in, EVP_aes_128_cbc(), PKCS7_BINARY);
 	if (!p7)
-		goto seal_pkcs7_envelop_err;
+		goto seal_pkcs7_envelope_err;
 
 	f_len = i2d_PKCS7(p7, &p_der);
 	fp = fopen(fout, "wb");
@@ -95,19 +95,105 @@ int seal_pkcs7_envelop(X509 *cert, char *fin, char *fout, void *msg)
 
 	return ret;
 
-seal_pkcs7_envelop_err:
+seal_pkcs7_envelope_err:
 	//your error handle
 	return ret;
 }
 
-//pkcs7_envelope [cert file] [null] [p7.der] [null]
+
+EVP_PKEY *parse_prikey(char *pkey_file)
+{
+	FILE *fp = NULL;
+	EVP_PKEY *pkey = NULL;
+
+	//TODO d2i_PrivateKey_fp(FILE *fp, EVP_PKEY **a);
+
+	fp = fopen(pkey_file, "r");
+	if (!fp)
+		goto parse_prikey_err;
+
+	if (!PEM_read_PrivateKey(fp, &pkey, NULL, NULL))
+		goto parse_prikey_err;
+
+	//TODO RSA private only
+
+	return pkey;
+
+parse_prikey_err:
+	//your error handle
+	printf("%s err out\n", __func__);
+	if (fp)
+		fclose(fp);
+	if (pkey)
+	{
+		EVP_PKEY_free(pkey);
+		pkey = NULL;
+	}
+	return pkey;
+}
+
+/**
+ * most situation private key already loaded to EVP_PKEY in buf
+ *
+ * Although the recipients certificate is not needed to decrypt
+ * the data it is needed to locate the appropriate (of possible several)
+ * recipients in the PKCS#7 structure.
+ *
+ */
+int open_pkcs7_envelope(EVP_PKEY *prikey, X509 *cert, char *fin, char *fout)
+{
+	int len, ret = 0;
+	unsigned char *p = NULL;
+	FILE *fp = NULL;
+	PKCS7 *p7 = NULL;
+	BIO *b_out = NULL;
+
+	fp = fopen(fin, "r");
+	if (!fp)
+		goto open_pkcs7_envelope_err;
+	p7 = d2i_PKCS7_fp(fp, &p7);
+	if (!p7)
+		goto open_pkcs7_envelope_err;
+	b_out = BIO_new(BIO_s_mem());
+	if (!b_out)
+		goto open_pkcs7_envelope_err;
+	//now open the envelope
+	if (1 != PKCS7_decrypt(p7, prikey, cert, b_out, PKCS7_BINARY))
+		goto open_pkcs7_envelope_err;
+
+	len = BIO_get_mem_data(b_out, &p);
+	printf("%d\n", len);
+	BIO_dump_fp(stdout, p, len); //debug
+
+	//TODO write to fout file
+
+	//free
+	fclose(fp);
+	PKCS7_free(p7);
+	BIO_free(b_out);
+
+	return ret;
+
+open_pkcs7_envelope_err:
+	//your error handle
+	printf("%s err out\n", __func__);
+	return ret;
+}
+
+//pkcs7_envelope [cert file] [private key file] [p7.der] [plain text]
 int main(int argc, char * argv[])
 {
 	int ret = 0;
 
+	//seal
 	X509 *c = parse_cert(argv[1]);
-	if (c)
-		seal_pkcs7_envelop(c, argv[2], argv[3], NULL);
+	// if (c)
+	// 	seal_pkcs7_envelope(c, NULL, argv[3], NULL);
+	
+	//open
+	EVP_PKEY *pkey = parse_prikey(argv[2]);
+	if (pkey)
+		open_pkcs7_envelope(pkey, c, argv[3], argv[4]);
 
 	return ret;
 }
